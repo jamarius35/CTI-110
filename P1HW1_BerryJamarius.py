e776486efcc85f4bf0046a1793610be48f785ae0
jamarius35 <berryj4698@student.faytechcc.edu>
2024-09-19 11:41:23 -0400

    P1HW1

---
+++ P1HW1_BerryJamarius.py
@@ -0,0 +1,37 @@
+# Jamarius Berry
+# 9/17/2024
+# P1HW1
+# Input and Output with mathematical expressions
+
+print("----Calculating Exponents----")
+print()
+print()
+
+# Get base value (as an integer) from the user
+base_value = int(input("Enter an integer as a base value: "))
+exponent = int(input("Enter an interger as the exponent: "))
+
+# Raise the base_value to the exponent
+value = base_value ** exponent
+
+print()
+print()
+
+# Display output to the user
+print(base_value, "raised to the power of", exponent, "is", value, "!!")
+print()
+print()
+print()
+
+
+print("----Addition and Subtraction----")
+print()
+print()
+start_num = int(input("Enter a starting integer: "))
+addition = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print()
+print()
+print(start_num, "+", addition, "-", subtract, "is equal to", start_num + addition - subtract)
+
+
